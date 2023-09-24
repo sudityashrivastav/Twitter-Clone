440bb11ab1225cc63e4f8226790aff6ad26120c4
sudityashrivastav <sudityakumar1@gmail.com>
2023-09-24 16:14:14 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'localhost',
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com'
+    ]
+  }
+};
+
+module.exports = nextConfig;
